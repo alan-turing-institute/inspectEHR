24de9a9b2b9e03fc7c9b7a01003d528161618597
Iain <25081046+Iain-S@users.noreply.github.com>
2022-04-07 23:39:57 +0100

    Use devtools to load package before testing

--- tests/testthat/test_perform_evaluation.R
+++ tests/testthat/test_perform_evaluation.R
@@ -3,7 +3,7 @@ test_that("we can run in .debug with a SQLite database", {
   ctn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
 
   expect_true(
-    inspectEHR::perform_evaluation(
+    devperform_evaluation(
       connection = ctn,
       output_folder = "./output/",  # note trailing slash
       verbose = TRUE,
