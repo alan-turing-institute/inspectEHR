f0d70df5a41c927c967fa8765e4185f6872fd1d0
Iain <25081046+Iain-S@users.noreply.github.com>
2022-04-07 23:32:38 +0100

    Use full name in tests

--- tests/testthat/test_perform_evaluation.R
+++ tests/testthat/test_perform_evaluation.R
@@ -3,7 +3,7 @@ test_that("we can run in .debug with a SQLite database", {
   ctn <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
 
   expect_true(
-    perform_evaluation(
+    inspectEHR::perform_evaluation(
       connection = ctn,
       output_folder = "./output/",  # note trailing slash
       verbose = TRUE,
