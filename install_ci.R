8b4756686e855b91af8ecd0d91dc62da0a6606f5
Iain <25081046+Iain-S@users.noreply.github.com>
2022-04-28 11:39:25 +0100

    Cache the installed R packages

--- install_ci.R
+++ install_ci.R
@@ -1,5 +1,5 @@
 # Install this package and its dependencies for C.I. testing
 install.packages("remotes")
-remotes::install_github("DocEd/commonEHR")
-remotes::install_github("DocEd/wranglEHR")
-remotes::install_local(".", dependencies=TRUE)
+remotes::install_github("DocEd/commonEHR", force=FALSE)
+remotes::install_github("DocEd/wranglEHR", force=FALSE)
+remotes::install_local(".", dependencies=TRUE, force=FALSE)
