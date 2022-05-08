2dad724d6bf07dad61c3dc626f63e1c0e156f31d
Iain <25081046+Iain-S@users.noreply.github.com>
2022-05-08 19:28:27 +0100

    Remove redundant lines

--- test_ci.R
+++ test_ci.R
@@ -1,11 +1,3 @@
 packageVersion("inspectEHR")
-results = testthat::test_file("tests/testthat/test_perform_evaluation.R")
+testthat::test_file("tests/testthat/test_perform_evaluation.R")
 
-# https://stackoverflow.com/a/61013209/3324095
-failed_tests <- sapply(results, function(r) {
-  !is(r$result[[1]], "expectation_success")
-})
-
-if (any(failed_tests)) {
-    quit(status=1)
-}
