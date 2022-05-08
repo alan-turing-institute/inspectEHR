4bf32ee3d391be96f68ab8162bcd4b467ec3a759
Iain <25081046+Iain-S@users.noreply.github.com>
2022-05-08 18:53:54 +0100

    Add a test db setup function

---
+++ tests/testthat/setup.R
@@ -0,0 +1,33 @@
+# Set up our test database
+setup_connection <- function() {
+  connection <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+
+  events <- .events %>%
+    dplyr::mutate(
+      datetime = strftime(datetime),
+      date = strftime(date, format = "%Y-%m-%d"),
+      time = strftime(time, format = "%H:%M:%S")) %>%
+    DBI::dbWriteTable(connection, "events", .)
+
+  episodes <- .episodes %>%
+    dplyr::mutate(
+      start_date = strftime(start_date)
+    ) %>%
+    DBI::dbWriteTable(connection, "episodes", .)
+
+  provenance <- .provenance %>%
+    dplyr::mutate(
+      date_created = strftime(date_created),
+      date_parsed = strftime(date_parsed),
+    ) %>%
+    DBI::dbWriteTable(connection, "provenance", .)
+
+  variables <- DBI::dbWriteTable(connection, "variables", .variables)
+
+  return(connection)
+}
+
+# Disconnect from our test database
+cleanup_connection <- function(connection) {
+  DBI::dbDisconnect(connection)
+}
